0e4353d62ada8cc6e314c14fe1ccfe4960b230a6
rickNoise <emberix@gmail.com>
2025-07-25 21:23:45 +1000

    slight refactor of main

--- main.go
+++ main.go
@@ -2,18 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/rickNoise/aggreGATOR/internal/config"
 )
 
 func main() {
 	// Read the config file.
-	c, _ := config.Read()
+	c, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config: %+v\n", *c)
 
 	// Set the current user to "lane" (actually, you should use your name instead) and update the config file on disk.
-	c.SetUser("nick")
+	err = c.SetUser("nick")
+	if err != nil {
+		log.Fatalf("could not set current user: %v", err)
+	}
 
 	// Read the config file again and print the contents of the config struct to the terminal.
-	c, _ = config.Read()
+	c, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
 	fmt.Println(*c)
 }
